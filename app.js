fb85054e12c7922bf3c644f28700307a7c526c32
Fred Tagle <fredtaglell@gmail.com>
2024-08-06 19:48:43 -0500

    correpcion de javascript

--- app.js
+++ app.js
@@ -2,59 +2,71 @@
 let resultado="";
 let texto=""
 let parrafo=document.querySelector("#btn__copiar");
+const comprobar =/^[a-z0-9\s]+$/;
 function textarea(){
     texto=document.querySelector(".encriptador__texto").value;
-    document.querySelector(".encriptador__texto").value="";    
+    document.querySelector(".encriptador__texto").value="";
+    
 }
+
 
 function encriptado(){
     textarea();
-    for (let letra of texto){
-        switch(letra){
-            case "a":
-                letra="ai";
-                break;
-            case "e":
-                letra="enter";
-                break;
-            case "i":
-                letra="imes";
-                break;
-            case "o":
-                letra="ober";
-                break;
-            case "u":
-                letra="ufat";
-                break;
-            default:
-                break;
+    if(comprobar.test(texto)){
+        for (let letra of texto){
+            switch(letra){
+                case "a":
+                    letra="ai";
+                    break;
+                case "e":
+                    letra="enter";
+                    break;
+                case "i":
+                    letra="imes";
+                    break;
+                case "o":
+                    letra="ober";
+                    break;
+                case "u":
+                    letra="ufat";
+                    break;
+                default:
+                    break;
+            }
+            resultado+=letra;
         }
-        resultado+=letra;
-    }
-    document.querySelector(".desencriptador__texto").value=resultado;
-    resultado=""
+        document.querySelector(".desencriptador__texto").value=resultado;
+        resultado=""
+        if(window.innerWidth <=375 && getComputedStyle(parrafo).visibility === "visible" )  {
+                document.querySelector("#btn__copiar").scrollIntoView({behavior:"smooth"});
+        }else{
+                document.querySelector(".logo").scrollIntoView({behavior:"smooth"});
     
-    if(window.innerWidth <=375 && getComputedStyle(parrafo).visibility === "visible" )  {
-        document.querySelector("#btn__copiar").scrollIntoView({behavior:"smooth"});
+        }
     }else{
-        document.querySelector(".logo").scrollIntoView({behavior:"smooth"});
-
+        alert("ERROR PORFAVOR INGRESE EL TEXTO TODO EN MINUSCULA Y SIN CARACTERES ESPECIALES");
+        resultado=""
     }
+    
 }
 
 function desencriptador(){
     textarea();
-    texto=texto.replace(/ai/g,"a");
-    texto=texto.replace(/enter/g,"e");
-    texto=texto.replace(/imes/g,"i");
-    texto=texto.replace(/ober/g,"o");
-    texto=texto.replace(/ufat/g,"u");
-    document.querySelector(".desencriptador__texto").value=texto;
-    texto="";
-    if(window.innerWidth <=375 && getComputedStyle(parrafo).visibility === "visible" )  {
-        document.querySelector("#btn__copiar").scrollIntoView({behavior:"smooth"});
+    if(comprobar.test(texto)){
+        texto=texto.replace(/ai/g,"a");
+        texto=texto.replace(/enter/g,"e");
+        texto=texto.replace(/imes/g,"i");
+        texto=texto.replace(/ober/g,"o");
+        texto=texto.replace(/ufat/g,"u");
+        document.querySelector(".desencriptador__texto").value=texto;
+        texto="";
+        if(window.innerWidth <=375 && getComputedStyle(parrafo).visibility === "visible" )  {
+            document.querySelector("#btn__copiar").scrollIntoView({behavior:"smooth"});
+        }else{
+            document.querySelector(".logo").scrollIntoView({behavior:"smooth"});
+        }
     }else{
-        document.querySelector(".logo").scrollIntoView({behavior:"smooth"});
+        alert("ERROR PORFAVOR INGRESE EL TEXTO TODO EN MINUSCULA Y SIN CARACTERES ESPECIALES");
     }
 }   
 function copiar(){
